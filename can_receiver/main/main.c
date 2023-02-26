aa743eb3569da71643687ce58f91543fa846b6fd
Alexandro C.W <alexandrowijaya.aw2@gmail.com>
2023-02-26 23:14:13 +0700

    Add mcp2515 driver

--- can_receiver/main/main.c
+++ can_receiver/main/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "Freertos/task.h"
+#include "esp_log.h"
+#include "mcp2515.h"
+
+static const char *TAG = "CAN";
+
+void receive_can_message() {
+  mcp2515_message_t message;
+  if (mcp2515_receive_message(&message)) {
+    ESP_LOGI(TAG, "CAN message received");
+    for (int i = 0; i < message.length; i++) {
+      ESP_LOGI(TAG, "data[$d]: $02X", i, message.data[i]);
+    }
+  }
+}
+
+void can_task(void *pvParameters) {
+  eSP_LOGI(TAG, "CAN task started");
+  
+  mcp2515_init();
+
+  while(1) {
+    receive_can_message();
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
 
 void app_main(void)
 {
-
+  xTaskCreate(&can_task, "can_task", 4096, NULL, 5, NULL);
 }
--- can_sender/main/main.c
+++ can_sender/main/main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "mcp2515.h"
+
+static const char *TAG = "CAN";
+
+void send_can_message(uint8_t data[], uint8_t len) {
+  mcp2515_message_t message;
+  message.sid = 0x123;
+  message.rtr = 0;
+  message.length = len;
+  memcpy(message.data, data, len);
+  mcp2515_send_message(&message);
+}
+
+void can_task(void *pvParameters) {
+  ESP_LOGI(TAG, "CAN task started");
+
+  mcp2515_init();
+
+  while(1) {
+    uint8_t data[] = {0x01, 0x02, 0x03};
+    send_can_message(data, sizeof(data));
+    ESP_LOGI(TAG, "CAN message sent");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
 
 void app_main(void)
 {
-
+  xTaskCreate(&can_task, "can_task", 4096, NULL, 5, NULL);
 }
